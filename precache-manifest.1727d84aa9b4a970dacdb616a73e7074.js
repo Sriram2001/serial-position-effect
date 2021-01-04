24490e6e39d90653274d64cbcbd138e81d983b4e
Sriram2001 <sriramudupi19@gmail.com>
2021-01-05 01:26:48 +0530

    Updates

---
+++ precache-manifest.1727d84aa9b4a970dacdb616a73e7074.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffed45bbd8f579509121328e0c49190e",
+    "url": "/serial-position-effect/index.html"
+  },
+  {
+    "revision": "b1d88cb7984208d93292",
+    "url": "/serial-position-effect/static/js/2.fb880f55.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/serial-position-effect/static/js/2.fb880f55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bfe84dba1032af4df63",
+    "url": "/serial-position-effect/static/js/main.c617d789.chunk.js"
+  },
+  {
+    "revision": "ee742c3ba88ab9d0bacc",
+    "url": "/serial-position-effect/static/js/runtime-main.3f7a69d1.js"
+  },
+  {
+    "revision": "b17ce998fed5bb4d4a6ff6686038a6d4",
+    "url": "/serial-position-effect/static/media/memory.b17ce998.png"
+  }
+]);
